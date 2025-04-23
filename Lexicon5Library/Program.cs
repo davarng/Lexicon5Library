5f1003b35cf1927774954fa5880eb0f75990dbdb
davarng <113583854+davarng@users.noreply.github.com>
2025-04-23 10:20:30 +0200

    Add project files.

---
+++ Lexicon5Library/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lexicon5Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
