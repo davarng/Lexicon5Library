43c3f5bf568b72641258419abff42e6cb750c623
davarng <113583854+davarng@users.noreply.github.com>
2025-04-25 14:55:50 +0200

    Finished search methods

--- Lexicon5Library/Program.cs
+++ Lexicon5Library/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Lexicon5Library.Json;
+using System;
+using System.Collections;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using System.Xml;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Lexicon5Library;
 
@@ -24,7 +27,7 @@ internal class Program
                 $"1. Create book{Environment.NewLine}" +
                 $"2. List books{Environment.NewLine}" +
                 $"3. Remove book{Environment.NewLine}" +
-                $"4. Borrow book{Environment.NewLine}" +
+                $"4. Search book{Environment.NewLine}" +
                 $"5. add category{Environment.NewLine}" +
                 $"6. remove category{Environment.NewLine}" +
                 $"7. login{Environment.NewLine}" +
@@ -32,7 +35,7 @@ internal class Program
                 $"9. user area{Environment.NewLine}" +
                 $"Q. Quit application.");
 
-            input = Console.ReadLine();
+            input = Console.ReadLine() ?? "";
 
             switch (input)
             {
@@ -46,7 +49,7 @@ internal class Program
                     RemoveBook(books);
                     break;
                 case "4":
-                    SearchForBook();
+                    BookSearchSelection(books);
                     break;
                 case "5":
 
@@ -96,14 +99,20 @@ internal class Program
     private static void AddBook(List<Book> books)
     {
         Console.Clear();
-        Console.Write("TITLE: ");
+        Console.Write("Title: ");
         string title = Console.ReadLine();
         Console.Write($"Author: ");
         string author = Console.ReadLine();
         Console.Write($"ISBN: ");
-        bool success = int.TryParse(Console.ReadLine(), out int isbn);
+        _ = int.TryParse(Console.ReadLine(), out int isbn);
         Console.Write($"Category: ");
         string category = Console.ReadLine();
+
+        if (books.Any(book => book.Isbn == isbn))
+        {
+            Console.WriteLine("A book with this isbn already exists...");
+            return;
+        }
 
         Book book = new(title, author, isbn, category);
         Console.WriteLine($"Book {title} created!{Environment.NewLine}");
@@ -129,10 +138,101 @@ internal class Program
     #endregion
 
 
-    static void SearchForBook()
+    static void BookSearchSelection(List<Book> books)
     {
-        throw new NotImplementedException();
+        Console.Clear();
+        Console.WriteLine($"What would you like to search by?{Environment.NewLine}" +
+            $"1. Author{Environment.NewLine}" +
+            $"2. Title{Environment.NewLine}" +
+            $"3. Isbn{Environment.NewLine}" +
+            $"4. Category");
+        string input = Console.ReadLine() ?? "";
+
+        switch (input)
+        {
+            case "1":
+                BookSearch(books, ByAuthor);
+
+                break;
+            case "2":
+                BookSearch(books, ByTitle);
+
+                break;
+            case "3":
+                BookSearch(books, ByIsbn);
+
+                break;
+            case "4":
+                BookSearch(books, ByCategory);
+
+                break;
+            default:
+                Console.WriteLine("Invalid search term.");
+
+                break;
+        }
     }
+
+    private static void BookSearch(List<Book> books, Func<List<Book>, string, List<Book>> searchBy)
+    {
+        Console.Write("Write word to search for: ");
+        string searchWord = Console.ReadLine() ?? "";
+        PrintList(searchBy(books, searchWord));
+    }
+
+    private static void BookSearch(List<Book> books, Func<List<Book>, int, List<Book>> searchBy)
+    {
+        Console.Write("Write word to search for: ");
+        bool success = int.TryParse(Console.ReadLine(),out int searchInt);
+        if(success)
+            PrintList(searchBy(books, searchInt));
+        else
+            Console.WriteLine("Not a valid isbn...");
+    }
+
+    private static List<Book> ByAuthor(List<Book> books, string searchWord)
+    {
+        var searchedBooks = books.Where(book => book.Author.Contains(searchWord)).ToList();
+        return searchedBooks;
+    }
+
+    private static List<Book> ByTitle(List<Book> books, string searchWord)
+    {
+        var searchedBooks = books.Where(book => book.Title.Contains(searchWord)).ToList();
+        return searchedBooks;
+    }
+
+    private static List<Book> ByIsbn(List<Book> books, int searchWord)
+    {
+        var searchedBooks = books.Where(book => book.Isbn == searchWord).ToList();
+        return searchedBooks;
+    }
+
+    private static List<Book> ByCategory(List<Book> books, string searchWord)
+    {
+        var searchedBooks = books.Where(book => book.Category.Contains(searchWord)).ToList();
+        return searchedBooks;
+    }
+
+    //Not using anymore.
+
+    //private static void BookSearchByAuthor(List<Book> books)
+    //{
+    //    Console.Write("Write a search term for author: ");
+    //    string searchAuthor = Console.ReadLine();
+    //    var searchedBooks = books.Where(book => book.Author.Contains(searchAuthor)).ToList();
+
+    //    PrintList(searchedBooks);
+    //}
+
+    //private static void BookSearchByTitle(List<Book> books)
+    //{
+    //    Console.Write("Write a search term for title: ");
+    //    string searchTitle = Console.ReadLine();
+    //    var searchedBooks = books.Where(book => book.Title.Contains(searchTitle)).ToList();
+
+    //    PrintList(searchedBooks);
+    //}
 
 
 
